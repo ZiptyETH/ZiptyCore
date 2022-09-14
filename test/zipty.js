6fb2fef9bed6d3dddeee6978b0ef36f382c74df9
Rolando Perez <me@rolilink.com>
2022-09-13 20:40:34 -0500

    Adding factory function to tests

--- test/zipty.js
+++ test/zipty.js
@@ -1,15 +1,17 @@
 const ZiptyCore = artifacts.require("Zipty");
 const { deployProxy } = require('@openzeppelin/truffle-upgrades');
 
+const deployZiptyV1 = async () => await deployProxy(ZiptyCore, { kind: "uups" });
+
 contract("Zipty V1", function () {
   it("Should contain the correct name", async function () {
-    const ziptyContract = await deployProxy(ZiptyCore, { kind: "uups" });
+    const ziptyContract = await deployZiptyV1();
     
     assert( await ziptyContract.name() === 'Zipty');
   });
 
   it("Should contain the correct symbol", async function () {
-    const ziptyContract = await deployProxy(ZiptyCore, { kind: "uups" });
+    const ziptyContract = await deployZiptyV1();
     
     assert( await ziptyContract.symbol() === 'ZPTY');
   });
